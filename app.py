2e51ae42851afbe1dda3bde60922b3a6a27da100
Marcos Silva <msilva@omniorthoandspine.com>
2024-07-15 11:43:48 -0400

    Updated Monday morning

--- app.py
+++ app.py
@@ -30,5 +30,47 @@ def get_tasks():
 
     return jsonify (output)
 
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    # task =None
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+        
+    return jsonify({"message": "Nao foi possivel encontrar a atividade"}), 404
+
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+    task = None
+    for t in tasks:
+        if t.id ==id:
+            task = t
+            break
+        
+    if task == None:
+        return jsonify({"message": "Nao foi possivel encontrar a atividade"}), 404
+    
+    data = request.get_json()
+    task.title = data['title']
+    task.description = data['description']
+    task.completed = data['completed']
+    print(task)
+    return jsonify({"message": "Tarefa atualizada com sucesso"})
+
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+
+        if not task:
+            return jsonify({"message": "Nao foi possivel encontrar a atividade"}), 404
+        
+        tasks.remove(task)
+        return jsonify({"message": "Tarefa deletada com sucesso"})
+
 if __name__ == "__main__":
     app.run(debug=True)
