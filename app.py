9d0dc04283180872e7ed92bc9ae3b8fcce2a8191
marcosiMacHome <marcospaulo02@gmail.com>
2024-07-15 09:05:53 -0400

    Sunday - Week 29

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello World!"
+tasks = []
+task_id_control = 1
 
-@app.route("/about")
-def about():
-    return "Pagina Sobre"
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id=task_id_control, title=data.get("title"), description=data.get("description", ""))
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message": "Nova tarefa criada com sucesso"})
+
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list = []
+    for task in tasks:
+        task_list.append(task.to_dict())
+        
+    output = {
+                "tasks": task_list,
+                "total_tasks": 0
+            }
+
+    return jsonify (output)
 
 if __name__ == "__main__":
     app.run(debug=True)
